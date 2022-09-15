f94e771a74cdfc5e0671e0df416e8a8047a3eaea
pedro saez <pedrosaez02@gmail.com>
2022-09-14 21:31:02 -0300

    quede en el refresh del carrito

--- main.js/main.js
+++ main.js/main.js
@@ -33,13 +33,21 @@ function buscarMueble(id){
 
 function agregarMuebles(id){
     const mueblesCarrito = cargarMueblesCarrito();
-    const mueble = buscarMueble(id);
-    mueblesCarrito.push(mueble);
+    let pos = mueblesCarrito.findIndex(item=> item.id === id);
+
+    if(pos > -1){
+        mueblesCarrito[pos].cantidad += 1;
+    }else{
+        const mueble = buscarMueble(id);
+        muebles.cantidad = 1;
+        mueblesCarrito.push(mueble);
+    }
+
     guardarMueblesCarrito(mueblesCarrito);
     botonCarrito();
 
 }
-// MINUTO 1.09
+
 // Render boton carrito
 
 function botonCarrito(){
@@ -54,7 +62,7 @@ function botonCarrito(){
 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 </div>
 <div class="offcanvas-body">
-    ...
+    ${renderMueblesCarrito()}
 </div>
 </div>
     `;
@@ -65,25 +73,31 @@ function botonCarrito(){
 // Render muebles en Carrito
 
 function renderMueblesCarrito(){
-    const muebles = cargarMueblesLs();
-    let catalogo = "";
+    const muebles = cargarMueblesCarrito();
+    let mueblesEnCarrito = "";
 
-    muebles.forEach(mueble => {
-        catalogo +=  
-        `<div class="col-md-3 justify-content-center">
-            <div class="card text-center border border-0">
-                <img src="./img/${mueble.imagen}" class="card-img-top" alt="${mueble.nombre}">
-                <div class="card-body">
-                    <h5 class="card-title">${mueble.nombre}</h5>
-                    <p class="card-text">$${mueble.precio}</p>
-                    <a href="#" class="agregarAlCarrito rounded-pill btn btn-outline-dark" title="Agregar" onclick="agregarMuebles(${mueble.id})" >Agregar al carrito</a>
-                </div>
-            </div>
-        </div>`;
+    if(muebles.length == 0){
+        mueblesEnCarrito = `<div class="alert alert-secondary" role="alert">No hay productos en el carrito!</div>`;
+    }else{
+        mueblesEnCarrito += `<table class="table">`;
         
-    });
+        muebles.forEach(mueble => {
+            mueblesEnCarrito +=  
+            `<tr>
+                    <td><img src="./img/${mueble.imagen}" class="imgVistaCarritoCompra" alt="${mueble.nombre}"></td>
+                    <td>${mueble.nombre}</td>
+                    <td>$${mueble.precio}</td>
+                </tr>
+                <tr>
+                    <td><a href = "#" class="btn btn-outline-dark ">-</a> ${muebles.cantidad} <a href = "#" class="btn btn-outline-dark ">+</a></td>
+                </tr>`;    
+        });
+        
+        mueblesEnCarrito += ` </table>`;
+    }
+    
 
-    document.getElementById("muebles").innerHTML = catalogo; 
+    return mueblesEnCarrito;
 }
 
 // Render Sección muebles
